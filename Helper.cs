ed6d4a3daa4228b8da4226d6c846edfded6b167c
Ronie de Jesus Nogueira <ronie.nogueira@outlook.com>
2023-03-09 14:02:12 -0300

    Commit inicial

---
+++ Helper.cs
@@ -0,0 +1,14 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+
+namespace XmlWordHelper
+{
+    public static class Helper
+    {
+        public static void ConverterParaPDF()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Packaging;
+using PdfSharpCore.Pdf;
+
+var docPath = @"C:/TESTE.docx";
+
+using (var document = WordprocessingDocument.Open(docPath, true))
+{
+	try
+	{
+		string textoBuscado = "NOMECOMPLETO";
+		string novoTexto = "RONIE DE JESUS NOGUEIRA";
+
+		var body = document.MainDocumentPart.Document.Body;
+		body.InnerXml = body.InnerXml.Replace(textoBuscado, novoTexto);
+
+		document.Save();
+	}
+	catch (Exception)
+	{
+		throw;
+	}
+	finally
+	{
+		document.Close();	
+	}
+}
